1f10127c48d8a2ffd3d42ff04fd9ed7fcfe123e9
Chris Pappas <pappas.christopher@gmail.com>
2021-12-02 10:25:52 -0600

    Starting on the basic API example from golang docs

---
+++ README.md
@@ -0,0 +1,10 @@
+# Grafana "Kitchen Sink" Example
+
+This project is a simple demo of how to use grafana plugins with a simple
+golang based http server backend, deployed and run using docker-compose
+
+## Go-based http server
+
+We will simply follow the [Developing a RESTful API with Go and Gin](https://go.dev/doc/tutorial/web-service-gin) 
+tutorial from the official golang docs site
+
---
+++ api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// album represents data about a record album
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data
+var albums = []album{
+	{ID: "1", Title: "Dookie", Artist: "Green Day", Price: 13.99},
+	{ID: "2", Title: "Aenima", Artist: "Tool", Price: 15.99},
+	{ID: "3", Title: "Road Apples", Artist: "The Tragically Hip", Price: 2.50},
+}
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
